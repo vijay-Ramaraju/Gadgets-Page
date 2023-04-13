e48cd8e7e4dd887af2633f91d6d484723f1dfb41
vijay-Ramaraju <vijayy2500@gmail.com>
2023-04-13 08:34:59 +0000

    commit message

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get("/gadgets", (request, response) => {
+    response.sendFile('./gadgets.html', { root: __dirname });
+
+});
+module.exports = app
